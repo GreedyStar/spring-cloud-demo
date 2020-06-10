b65138496ed4986644e2cb2b10e364c4336dfc60
Alvin <greedyalvin@163.com>
2020-06-10 09:23:54 +0800

    Zuul Gateway

---
+++ sample-3/zuul-gateway/src/main/java/com/greedystar/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.greedystar.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Author GreedyStar
+ * Date   2020-6-10
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+
+}
